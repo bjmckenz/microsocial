f7ec9817913084b0fca5e36594ede9c7e0c9ef8e
Nathaniel Min <133430315+CSNateMin@users.noreply.github.com>
2023-05-19 04:33:45 -0700

    Got the DB, and Implementation of the 2 FA method with the four categories (none, auth, call and sms)

--- user/db.js
+++ user/db.js
@@ -1,36 +1,33 @@
-const Database = require('better-sqlite3')
-var db = new Database('./users.db')
+const sqlite3 = require('sqlite3').verbose();
+const faker = require('faker');
 
-// first time, or if we delete/reset users.db
-db.exec(`CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE COLLATE NOCASE,
-        password TEXT NOT NULL,
-        versionkey INTEGER NOT NULL DEFAULT 1
-    );`)
-db.exec(`CREATE TABLE IF NOT EXISTS users_result_sets (
-        set_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        set_session_id TEXT,
-        set_querying_user_id INTEGER,
-        set_results_as_of TEXT NOT NULL,
-        set_rownum INTEGER,
-        id INTEGER,
-        name TEXT,
-        versionkey INTEGER 
-    );`)
-db.exec(`CREATE TABLE IF NOT EXISTS refresh_tokens (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER NOT NULL UNIQUE,
-        refresh_token TEXT NOT NULL, 
-        issued TEXT,
-        expires TEXT
-    )`)
+// Connect to the database
+const db = new sqlite3.Database('test.db');
 
-// I have no idea why I had to do this. The get() is NOT defined in my DB for some reason. This polyfills it. BJM 4/15/23
-db.get = (stmt, params) => {
-  prep = db.prepare(stmt)
-  results = prep.all(params)
-  return results ? results[0] : undefined
+// Create a table
+db.run(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    phone TEXT
+  )
+`);
+
+// Generate test data
+for (let i = 0; i < 5; i++) {
+  const name = faker.name.findName();
+  const email = faker.internet.email();
+  const phone = faker.phone.phoneNumber();
+
+  // Insert data into the table
+  db.run(
+    'INSERT INTO users (name, email, phone) VALUES (?, ?, ?)',
+    [name, email, phone]
+  );
 }
 
-module.exports.db = db
+// Close the database connection
+db.close();
+
+console.log("Test data generated successfully!");

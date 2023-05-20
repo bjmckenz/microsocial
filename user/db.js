94888fc22d7f49c39779bc76fb71017624fb3270
arrowfyre <arrowfyre@fourthTry.myguest.virtualbox.org>
2023-05-19 17:22:07 -0700

    added email, recovery email, and country to user

--- user/db.js
+++ user/db.js
@@ -6,7 +6,10 @@ db.exec(`CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL UNIQUE COLLATE NOCASE,
         password TEXT NOT NULL,
-        versionkey INTEGER NOT NULL DEFAULT 1
+        versionkey INTEGER NOT NULL DEFAULT 1,
+        email varchar(255),
+        country varchar(255),
+        recovery_email varchar(255)
     );`)
 db.exec(`CREATE TABLE IF NOT EXISTS users_result_sets (
         set_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,9 +31,9 @@ db.exec(`CREATE TABLE IF NOT EXISTS refresh_tokens (
 
 // I have no idea why I had to do this. The get() is NOT defined in my DB for some reason. This polyfills it. BJM 4/15/23
 db.get = (stmt, params) => {
-  prep = db.prepare(stmt)
-  results = prep.all(params)
-  return results ? results[0] : undefined
+    prep = db.prepare(stmt)
+    results = prep.all(params)
+    return results ? results[0] : undefined
 }
 
 module.exports.db = db
